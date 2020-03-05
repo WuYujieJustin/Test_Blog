ea5cbe7449f33a6baf19183b316b23a81f4edcdf
WuYujie <wuyujie@zhongbiao.mobi>
2020-03-05 11:04:02 +0800

    init project

---
+++ src/components/ListCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="list-item">
+        <div>card</div>
+        <div>{{ card }}</div>
+        <div ref="editor" id="editor"></div>
+    </div>
+</template>
+
+<script>
+import * as Monaco from 'monaco-editor'
+export default {
+    name: 'ListCard',
+    props: {
+        card: {
+            type: Object
+        }
+    },
+    created() {
+        Monaco.create(this.$refs.editor)
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.list-item {
+    width: 100px;
+    height: 100px;
+    background: red;
+    #editor {
+        width: 200px;
+        height: 200px;
+    }
+}
+</style>
